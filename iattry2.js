5c7a9b9b1bb0ddaae09d474cae9680602be3b893
sshmalow <sabashah2310@gmail.com>
2024-07-18 14:29:33 +1200

    Update iattry2.js

--- iattry2.js
+++ iattry2.js
@@ -1,6 +1,6 @@
 // Your JavaScript code here
 Qualtrics.SurveyEngine.addOnload(function() {
-   define(['pipAPI', 'https://your-raw-github-url-here'], function(APIConstructor, iatExtension) {
+   define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension) {
       var API = new APIConstructor();
       var iat = new iatExtension({
          category1: {
